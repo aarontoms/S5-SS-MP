3532f89c0cad7b87bb21deac45c9721ae42f531e
Aaron Tom Viji <aarontoms29@gmail.com>
2024-11-10 20:48:31 +0530

    Add files via upload
    fix error

--- absolute loader/abs.c
+++ absolute loader/abs.c
@@ -3,10 +3,9 @@
 #include <string.h>
 
 int main() {
-    int max_size = 16;
-    int start, end, addr, length, ptr=0, prog_len;
+    int start, end, addr, length, prog_len;
     FILE *finput, *foutput;
-    char line[256], *token, objectcodes[360], onebyte[10];
+    char line[256], *token;
 
     finput = fopen("input.txt", "r");
     foutput = fopen("output.txt", "w");
@@ -14,18 +13,17 @@ int main() {
     fgets(line, 256, finput);
     token = strtok(line, "^");
     token = strtok(NULL, "^");
-    token = strtok(NULL, "^");              //token 3 times to get the starting address
+    token = strtok(NULL, "^");  //token 3 times to get the starting address
     addr = strtol(token, NULL, 16);
-    token = strtok(NULL, "^");              //token again to get the program length
+    token = strtok(NULL, "^");  //token again to get the program length
     end = strtol(token, NULL, 16);
     prog_len = end-start;
-    char objcodesArr[400][3];
     
     while(fgets(line, 256, finput)!=NULL){
         if(line[strlen(line)-1]=='\n'){
             line[strlen(line)-1] = 0; 
         }
-        token = strtok(line, "^");          //token to get whether T or E record type
+        token = strtok(line, "^");  //token to get whether T or E record type
         if(line[0] == 'T'){
             token = strtok(NULL, "^");
             start = strtol(token, NULL, 16);
@@ -34,7 +32,7 @@ int main() {
             }
 
             token = strtok(NULL, "^");
-            token = strtok(NULL, "^");      //token 2 times to access the object codesssss
+            token = strtok(NULL, "^");  //token 2 times to access the object codesssss
             while(token!=NULL){
                 length = strlen(token)/2;
                 fprintf(foutput, "%X\t%s\n", addr, token);
@@ -45,8 +43,5 @@ int main() {
         else if(line[0] == 'E'){
             printf("end");
         }
-    }
-    for(int i=0; i<ptr; i++){
-        printf("%s\n", objcodesArr[i]);
     }
 }
